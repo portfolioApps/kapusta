cfe64948f008b6f81f6a02ed48a735cae067a1ef
valery23 <valerka6501@gmail.com>
2023-10-06 21:22:48 +0300

    Update ServicesRoom.jsx

---
+++ ui/src/screens/services/ServicesRoom.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './ServicesRoom.module.css';
+
+
+
+const ServicesRoom = () => {
+    return(
+        <div className="post">
+
+        </div>
+    )
+}
+
+export default ServicesRoom;
